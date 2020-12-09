4c323d2b8d284c815671e5c6ef180ca4715e9f02
jokester4500 <kah90@students.uwf.edu>
2020-12-08 20:29:08 -0500

    test

--- activity2.js
+++ activity2.js
@@ -1,2 +1,3 @@
 
-console.log("Hello " + process.env.NAME.SECRET + " in " + process.env.YEAR + " years, you will be " + process.env.AGE.SECRET + " ! Won't you feel old?");
+//console.log("Hello " + process.env.NAME.SECRET + " in " + process.env.YEAR + " years, you will be " + process.env.AGE.SECRET + " ! Won't you feel old?");
+console.log("Hello");
