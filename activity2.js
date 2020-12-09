b2a13a7a802a00ab12f9d2e7aae43dad3881cb7d
jokester4500 <kah90@students.uwf.edu>
2020-12-09 00:49:29 -0500

    test

--- activity2.js
+++ activity2.js
@@ -1,3 +1,5 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
 
 //console.log("Hello " + process.env.NAME.SECRET + " in " + process.env.YEAR + " years, you will be " + process.env.AGE.SECRET + " ! Won't you feel old?");
 console.log("Hello");
